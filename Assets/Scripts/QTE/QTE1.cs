1868798ee5fcd4180bb69bae2ac4fd2d725deac2
vroman02 <vroman02@ucm.es>
2026-03-05 10:46:37 +0100

    Barra empezada
    He creado una escena de prueba para el qte 1 pero está sin acabar

---
+++ Assets/Scripts/QTE/QTE1.cs
@@ -0,0 +1,99 @@
+//---------------------------------------------------------
+// Script que se encarga de llenar la barra cuando pulsas la tecla asignada
+// Víctor Román Román
+// Nombre del juego
+// Proyectos 1 - Curso 2025-26
+//---------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UIElements;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class QTE1 : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+
+    /// <summary>
+    /// Cantidad que aumenta la barra por click del botón asignado.
+    /// </summary>
+    [SerializeField]
+    private float AumentoPorCLick = 5.0f;
+    /// <summary>
+    /// Componente de la barra
+    /// </summary>
+    [SerializeField]
+    private Slider ComponenteBarra;
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+        ComponenteBarra.value = 50;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if(InputManager.Instance.JumpWasPressedThisFrame())
+        {
+            ComponenteBarra.value += AumentoPorCLick;
+        }
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+    #endregion
+    
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    #endregion   
+
+} // class QTE1 
+// namespace
