3e03f30243a5a896f04758f1942c392f30a71bcb
vroman02 <vroman02@ucm.es>
2026-04-17 12:32:04 +0200

    Script Repair comentado y GetComponents corregidos
    Todos los GetComponents han sido declarados como variable e inicializados en el start. Las partes del código donde se hacía GetComponent ahora sólo usan la variable que toque.

--- Assets/Scripts/QTE/Manivela_QTE.cs
+++ Assets/Scripts/QTE/Manivela_QTE.cs
@@ -17,7 +17,7 @@ using UnityEngine.UI;
 /// Antes de cada class, descripción de qué es y para qué sirve,
 /// usando todas las líneas que sean necesarias.
 /// </summary>
-public class QTE2 : MonoBehaviour
+public class Manivela_QTE : MonoBehaviour
 {
     // ---- ATRIBUTOS DEL INSPECTOR ----
     #region Atributos del Inspector (serialized fields)
